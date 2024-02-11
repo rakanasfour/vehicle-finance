0aec7129f2af1c79f249a31c4b4ed1150fc6c0dd
rakanasfour <rjor4@hotmail.com>
2024-02-11 02:38:54 -0800

    small modification

--- app/data_access/database.py
+++ app/data_access/database.py
@@ -4,6 +4,6 @@ db = SQLAlchemy()
 
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Rakan@1992@localhost:3306/vehicle-finance'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:write-the-password@localhost:3306/vehicle-finance'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
